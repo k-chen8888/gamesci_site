b7d1331e81da0aff28a8ac85201ff9a3b1f92239
Kevin Chen <k.chen8880@gmail.com>
2016-01-31 06:47:48 -0500

    Blog posts not rendering
    Jade 'Cannot read property of undefined'

--- modules/blog/blog.js
+++ modules/blog/blog.js
@@ -11,6 +11,7 @@ module.exports = function(util, pages, secret) {
 		else {
 			if (exports.db) {
 				exports.db.BlogPost.getBlogPost({}, {post_id: -1}).then(function(posts) {
+					console.log(posts);
 					res.render(pages.blog + 'index', {posts: posts});
 				}, function (err) {
 					res.sendFile(pages.blog + 'index.html');

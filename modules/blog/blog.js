b7f6a099dfc7dc7faf9385123255208cc133ae43
Kevin Chen <k.chen8880@gmail.com>
2016-02-06 14:36:46 -0500

    Trying to make the blog page look better
    Also added a folder to host downloads

--- modules/blog/blog.js
+++ modules/blog/blog.js
@@ -11,6 +11,18 @@ module.exports = function(util, pages, secret) {
 		else {
 			if (exports.db) {
 				exports.db.BlogPost.getBlogPost({}, {post_id: -1}).then(function(posts) {
+					// Generate summaries for each post
+					posts = posts.map((curr, i, arr) => {
+						// Take about 75% of the contents of the first <p> tag to be the summary, without losing any tags
+						var summary = curr.body.substring(curr.body.indexOf('>', curr.body.indexOf('<p')) + 1, curr.body.indexOf('</p>'));
+						
+						return {
+							title: curr.title,
+							post_id: curr.post_id,
+							summary: '<p class="summary">' + fixTags(summary) + '[<a href="/blog?id="' + curr.post_id + '>...</a>]</p>';
+						};
+					});
+					
 					res.render(pages.blog + 'index', {posts: posts});
 				}, function (err) {
 					res.sendFile(pages.blog + 'index.html');
@@ -54,6 +66,39 @@ module.exports = function(util, pages, secret) {
 				res.redirect('/blog'); // Go back to the list of blog posts
 			});
 		} else res.redirect('/blog'); // Go back to the list of blog posts
+	};
+	
+	
+	/* Utilities */
+	
+	// Adds in missing tags
+	var fixTags = function(src) {
+		var openTags = [];
+		
+		// Detect all open tags
+		var openTagRE = /<[A-Za-z0-9 \.\"=]*>/g,
+			temp;
+		while ((temp = openTagRE.exec(src)) !== null) {
+			// Parse out and save the name of the tag
+			var tagName = temp[0].substring(1, temp[0].length - 1);
+			openTags.push(tagName.split(' ')[0]);
+		}
+		
+		// Remove tags from the list if they are closed
+		var closeTagRE = /<\/[A-Za-z0-9 \.]*>/g,
+			openTagIndex = -1;
+		while ((temp = closeTagRE.exec(src)) !== null) {
+			// If the tag is closed, remove it from the list
+			openTagIndex = openTags.indexOf(temp[0].substring(2, temp[0].length - 1));
+			if (openTagIndex > -1) openTags.splice(openTagIndex, 1);
+		}
+		
+		// In reverse order found, close all tags
+		var endTags = openTags.reduce((prev, curr, i, arr) => {
+			return '</' + curr + '>' + prev;
+		}, "");
+		
+		return src + endTags;
 	};
 	
 	
